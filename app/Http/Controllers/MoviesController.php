11fd828482c4c37e7db8f90cfc040c8d46a9b045
Muhammad Maaz Nizam <94466009+maaznizam290@users.noreply.github.com>
2023-03-08 19:36:28 +0500

    added record section of movies entries

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MoviesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::latest()->paginate(4);
+        return view('movies.index', compact('movies'))->with('i', (request('page', 1) - 1) * 4);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $genres = ['Action', 'Comedy', 'Horror', 'Biopic', 'Drama'];
+        return view('movies.create', compact('genres'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(['title' => 'required', 'genre' => 'required', 'poster' => 'required|image|mimes:png,jpg,gif|max:2048']);
+        $imageName = '';
+        if ($request->poster) {
+            $imageName = time() . '.' . $request->poster->extension();
+            $request->poster->move(public_path('uploads'), $imageName);
+        }
+        $data = new Movie;
+        $data->title = $request->title;
+        $data->genre = $request->genre;
+        $data->release_year = $request->release_year;
+        $data->poster = $imageName;
+        $data->save();
+        return redirect()->route('movies.index')->with('success', 'Movie had been added successfully now');
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        //
+    }
+}
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,32 @@
+@extends('movies.layout')
+@section('content')
+<div class="wrapperdiv">
+<table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Release Year</th>
+          </tr>
+        </thead>
+        @if ($movies)
+        <tbody>
+            @foreach ($movies as $movie)
+            <tr>
+                <td class="align-middle"><img src="{{asset('uploads/'.$movie->poster)}}" class="img-thumbnail"></td>
+                <td class="align-middle">{{$movie->title}}</td>
+                <td class="align-middle">{{$movie->genre}}</td>
+                <td class="align-middle">{{$movie->release_year}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+        @endif
+    </table> 
+    <div class="d-flex">
+        <div class="mx-auto">
+            {!! $movies->links() !! ?}
+        </div>
+    </div>
+    </div>
+      @endsection
